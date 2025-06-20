5ac28964562f58a47c3aed03357d73e63b349cdb
Bartlomiej Ciszewski <bartektricks@gmail.com>
2025-06-20 16:49:49 +0200

    Use simpler approach and update video one by one

---
+++ src/routes/compare/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Video from './Video.svelte';
+</script>
+
+<aside></aside>
+<div class="grid h-screen auto-rows-fr justify-items-center md:grid-cols-2">
+	<Video />
+	<Video />
+</div>
---
+++ src/routes/compare/Slider.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { cn } from '$lib/cn';
+	import { Slider, type SliderRootProps } from 'bits-ui';
+
+	let {
+		class: className,
+		value = $bindable(),
+		ref = $bindable(),
+		...props
+	}: SliderRootProps = $props();
+</script>
+
+<Slider.Root
+	{...props}
+	class={cn(
+		'invisible absolute right-0 bottom-0 left-0 z-10 flex h-4 w-full cursor-pointer items-center bg-zinc-900/20 select-none group-hover/video:visible',
+		className
+	)}
+>
+	<Slider.Range class="h-1/2 bg-white" />
+	<Slider.Thumb index={0} class="size-4 rounded-full bg-zinc-900" />
+</Slider.Root>
